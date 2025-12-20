d0d051c7aee3712f171482a6eff89083f09d5593
xcom20010 <morozov_alex2009@mail.ru>
2025-12-20 17:03:44 +0300

    created srever test script

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"fmt"
+
+	"github.com/gorilla/mux"
+)
+
+type GetResponse struct {
+	Message string `json:message`
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(GetResponse{Message: "Hello"})
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", getHandler).Methods("GET")
+	fmt.Println("Starting...")
+	http.ListenAndServe(":8080", r)
 }
