cffcbe819e02a7e6966ff1158398bdc927699b12
xcom20010 <morozov_alex2009@mail.ru>
2025-12-20 18:13:32 +0300

    I'm tired

--- main.go
+++ main.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
-
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 type GetResponse struct {
-	Message string `json:message`
+	Message string `json:"message"`
+}
+
+type PostRequest struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type PostResponse struct {
+	Info string      `json:"message"`
+	Data PostRequest `json:"data"`
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	var req PostRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		log.Fatalln("Ошибка", err)
+	}
+
+	createdReq := req
+	fmt.Println("Успешно создали переменную", createdReq)
+
+	resp := PostResponse{
+		Info: "Переменная создалась",
+		Data: createdReq,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +50,9 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", getHandler).Methods("GET")
-	fmt.Println("Starting...")
+	r.HandleFunc("/get", getHandler).Methods("GET")
+	r.HandleFunc("/post", postHandler).Methods("POST")
+
+	fmt.Println("Server is running...")
 	http.ListenAndServe(":8080", r)
 }
